dae99fd0d2addee2bfddb3d94bb76feaa21161fb
arthurpiratas <silva.arthursoares@gmail.com>
2021-08-11 20:43:23 -0300

    Incluindo rota para salvar categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,33 @@
 const express = require("express")
 const router = express.Router()
+const Category = require('./Category')
+const slugify = require("slugify")
+
+var data = new Date() // Capture a data universal
+var dataBrasilia = new Date(data.valueOf() - data.getTimezoneOffset() * 60000)
 
 router.get('/admin/categories/new', (req, res) => {
     res.render("admin/categories/new")
+})
+
+router.post('/categories/save', (req, res) => {
+    let title = undefined
+    title = req.body.title
+    console.log( "a" + title)
+    if(title != undefined && title != ""){
+
+        Category.create({
+            title: title,
+            slug: slugify(title),
+            createdAt: dataBrasilia,
+            updatedAt: dataBrasilia
+        }).then(() => {
+            res.redirect("/")
+        })
+
+    }else{
+        res.redirect("/admin/categories/new")
+    }
 })
 
 module.exports = router
